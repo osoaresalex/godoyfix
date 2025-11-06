b0335bd8aa403a578f356992255d9a9cdb24b055
Alex <multiwebagencia@gmail.com>
2025-11-06 18:42:09 -0300

    Add optimize:clear and optimize to Railway startup

--- Painel/railway-start.sh
+++ Painel/railway-start.sh
@@ -6,9 +6,12 @@ php artisan cache:clear
 php artisan view:clear
 php artisan config:clear
 php artisan route:clear
+php artisan optimize:clear
 
 # Rebuild caches
 php artisan config:cache
 php artisan route:cache
+php artisan optimize
+
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=$PORT
