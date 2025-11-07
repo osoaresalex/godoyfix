c85a140c24edaac006d70a39cdc3ae1325379afc
Alex <multiwebagencia@gmail.com>
2025-11-07 09:05:46 -0300

    Make force-recompile more aggressive: touch all blade files to force rebuild

--- Painel/force-recompile-views.php
+++ Painel/force-recompile-views.php
@@ -1,54 +1,71 @@
 <?php
-// Force recompile all Blade views
+// AGGRESSIVE force recompile all Blade views
 
+echo "========================================\n";
+echo "FORCING VIEW RECOMPILATION\n";
+echo "========================================\n\n";
+
+// Delete ALL compiled views BEFORE loading Laravel
+$viewPath = __DIR__ . '/storage/framework/views';
+echo "1. Deleting compiled views from: $viewPath\n";
+
+if (is_dir($viewPath)) {
+    $files = glob($viewPath . '/*');
+    $deleted = 0;
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+            $deleted++;
+        }
+    }
+    echo "   Deleted $deleted compiled view files\n\n";
+}
+
+// Delete bootstrap cache
+$bootstrapCache = __DIR__ . '/bootstrap/cache';
+if (is_dir($bootstrapCache)) {
+    $files = glob($bootstrapCache . '/*.php');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+            echo "   Deleted: " . basename($file) . "\n";
+        }
+    }
+}
+
+echo "\n2. Loading Laravel...\n";
 require __DIR__.'/vendor/autoload.php';
 
 $app = require_once __DIR__.'/bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-// Delete all compiled views
-$viewPath = storage_path('framework/views');
-echo "Deleting compiled views from: $viewPath\n";
-
-if (is_dir($viewPath)) {
-    $files = glob($viewPath . '/*');
-    foreach ($files as $file) {
-        if (is_file($file)) {
-            unlink($file);
-            echo "Deleted: $file\n";
-        }
-    }
-}
-
-// Clear view cache
+// Clear ALL Laravel caches
+echo "\n3. Clearing all Laravel caches...\n";
+Artisan::call('cache:clear');
+echo "   Cache cleared\n";
 Artisan::call('view:clear');
-echo "View cache cleared!\n";
+echo "   View cache cleared\n";
+Artisan::call('config:clear');
+echo "   Config cache cleared\n";
+Artisan::call('route:clear');
+echo "   Route cache cleared\n";
 
-// Get all blade files and force recompilation
-$bladeFiles = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator(base_path('Modules'))
+// Touch all blade files to make them "newer" than any compiled versions
+echo "\n4. Touching all Blade files to force recompilation...\n";
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator(base_path('Modules'), RecursiveDirectoryIterator::SKIP_DOTS),
+    RecursiveIteratorIterator::SELF_FIRST
 );
 
-$recompiled = 0;
-foreach ($bladeFiles as $file) {
-    if ($file->isFile() && $file->getExtension() === 'php' && strpos($file->getFilename(), '.blade.php') !== false) {
-        $viewPath = str_replace(base_path(), '', $file->getPathname());
-        $viewPath = str_replace(['\\', '/'], '.', $viewPath);
-        $viewPath = str_replace('.Modules.', '', $viewPath);
-        $viewPath = str_replace('.Resources.views.', '::', $viewPath);
-        $viewPath = str_replace('.blade.php', '', $viewPath);
-        
-        try {
-            // Try to render the view to force compilation
-            view($viewPath)->render();
-            $recompiled++;
-            echo "Recompiled: $viewPath\n";
-        } catch (\Exception $e) {
-            // Skip views that can't be rendered without data
-        }
+$touched = 0;
+foreach ($iterator as $file) {
+    if ($file->isFile() && strpos($file->getFilename(), '.blade.php') !== false) {
+        touch($file->getPathname());
+        $touched++;
     }
 }
+echo "   Touched $touched Blade files\n";
 
-echo "\n=== SUMMARY ===\n";
-echo "Total views recompiled: $recompiled\n";
-echo "Done!\n";
+echo "\n========================================\n";
+echo "DONE! Views will be recompiled on next request.\n";
+echo "========================================\n";
