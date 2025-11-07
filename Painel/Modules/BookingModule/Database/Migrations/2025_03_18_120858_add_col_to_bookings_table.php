e5fd27f0aec744d764c38c443718558c74b7f623
Alex <multiwebagencia@gmail.com>
2025-11-07 14:10:52 -0300

    fix(migration): rename duplicate AddColToBookingsTable class to AddServiceAddressLocationToBookingsTable

--- Painel/Modules/BookingModule/Database/Migrations/2025_03_18_120858_add_col_to_bookings_table.php
+++ Painel/Modules/BookingModule/Database/Migrations/2025_03_18_120858_add_col_to_bookings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColToBookingsTable extends Migration
+class AddServiceAddressLocationToBookingsTable extends Migration
 {
     /**
      * Run the migrations.
